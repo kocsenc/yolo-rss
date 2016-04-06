02992da6b3cadc4ac27bc1f8be52ffabb426d853
Kocsen <kocsenc@gmail.com>
2016-04-06 13:13:50 -0400

    Add sanitize and ycombinator

--- js/main.js
+++ js/main.js
@@ -7,29 +7,59 @@
  * # MainCtrl
  * Controller of the hayboys app
  */
-var app = angular.module('yolorss', ['ngResource', 'ngAnimate', 'ngCookies']);
+var app = angular.module('yolorss', ['ngResource', 'ngAnimate', 'ngCookies', 'ngSanitize']);
 
 
 angular
   .module('yolorss')
-  .controller('main', ['$resource', '$cookies', MainCtrl]);
+  .controller('main', ['$resource', '$cookies', 'FeedService', MainCtrl]);
 
-function MainCtrl($resource, $cookies) {
+function MainCtrl($resource, $cookies, FeedService) {
   var vm = this;
+  vm.loading = true;
+
+  FeedService.parseFeed('http://news.ycombinator.com/rss').then(function (res) {
+    vm.loading = false;
+
+    console.log(res);
+
+    var responseStatus = res.data.responseStatus;
+    var f = res.data.responseData.feed;
+
+    vm.feed = {
+      title: f.title,
+      link: f.link,
+      description: f.description,
+      type: f.type,
+      posts: f.entries
+    };
+
+    /**
+     * Posts have:
+     * - title
+     * - link
+     * - author
+     * - publishedDate
+     * - content (html)
+     * -
+     */
+  });
+
 
   setLastVisited();
-
   function setLastVisited() {
     var key = 'lastVisit';
     if (!$cookies.get(key)) {
-      console.log('Saving Cookie: ', new Date());
       $cookies.put(key, new Date().toString());
     }
     vm.lastVisit = $cookies.get(key);
-
-    alert($cookies.get(key));
   }
-
-  vm.potato = 'YOLO';
-
 }
+
+app.factory('FeedService', ['$http', function ($http) {
+  return {
+    parseFeed: function (url) {
+      return $http.jsonp('//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=50&callback=JSON_CALLBACK&q=' + encodeURIComponent(url));
+    }
+  }
+}]);
