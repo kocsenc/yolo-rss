599f8333a8b415223c8ce77100fbc05c2e9a1f3e
Kocsen <kocsenc@gmail.com>
2016-04-07 13:34:37 -0400

    Implement favorite

--- js/main.js
+++ js/main.js
@@ -16,53 +16,92 @@ angular
 
 function MainCtrl($resource, $cookies, FeedService) {
   var vm = this;
-  vm.loading = true;
 
+  /* Variables */
   var rssFeeds = [
     {
-      title: 'CNN',
-      url: 'http://rss.cnn.com/rss/cnn_topstories.rss'
+      title: 'USA Today',
+      url: 'http://rssfeeds.usatoday.com/usatoday-NewsTopStories'
     },
     {
-      title: 'Hacker News',
-      url: 'http://news.ycombinator.com/rss'
+      title: 'BBC World News',
+      url: 'http://feeds.bbci.co.uk/news/world/rss.xml'
+    },
+    {
+      title: 'ESPN Headlines',
+      url: ' http://sports.espn.go.com/espn/rss/news'
+    },
+    {
+      title: 'The Weather Channel®',
+      url: 'http://rss.weather.com/rss/national/rss_nwf_rss.xml?cm_ven=NWF&cm_cat=rss&par=NWF_rss'
     },
     {
       title: 'TechCrunch',
       url: 'http://feeds.feedburner.com/TechCrunch'
     }
-
   ];
 
-  FeedService.parseFeed(rssFeeds[2].url).then(function (res) {
-    vm.loading = false;
+  vm.categorySelected = 0;
 
-    console.log(res);
+  /* Functions */
+  vm.selectCategory = selectCategory;
+  vm.favorite = favorite;
 
-    var responseStatus = res.data.responseStatus;
-    var f = res.data.responseData.feed;
-
-    vm.feed = {
-      title: f.title,
-      link: f.link,
-      description: f.description,
-      type: f.type,
-      posts: f.entries
-    };
-
-    /**
-     * Posts have:
-     * - title
-     * - link
-     * - author
-     * - publishedDate
-     * - content (html)
-     * -
-     */
-  });
-
-
+  /* Init */
+  selectCategory(0);
   setLastVisited();
+
+  /**
+   * Retrieves the appropriate feed and populates it.
+   * @param number
+   */
+  function selectCategory(number) {
+    vm.loading = true;
+    vm.categorySelected = number;
+
+    FeedService.parseFeed(rssFeeds[number].url).then(function (res) {
+      vm.loading = false;
+      var responseStatus = res.data.responseStatus;
+      var f = res.data.responseData.feed;
+      vm.feed = {
+        title: f.title,
+        link: f.link,
+        description: f.description,
+        type: f.type,
+        posts: f.entries
+      };
+      populateFavorites(vm.feed.posts);
+
+      /**
+       * Posts have:
+       * - title
+       * - link
+       * - author
+       * - publishedDate
+       * - content (html)
+       * -
+       */
+    });
+  }
+
+  function populateFavorites(posts) {
+    angular.forEach(posts, function (post) {
+      var postCookie = $cookies.getObject(post.link);
+      if (postCookie) {
+        post.favorited = postCookie;
+      }
+    });
+  }
+
+  /**
+   * Toggles favorite of a post.
+   * @param post
+   */
+  function favorite(post) {
+    post.favorited = !post.favorited;
+    $cookies.put(post.link, post.favorited);
+  }
+
   function setLastVisited() {
     var key = 'lastVisit';
     if (!$cookies.get(key)) {
